4e718f63735432d1d67ea9d02fba797fe95c1cb7
nguyenminhtuan100802 <139602897+nguyenminhtuan100802@users.noreply.github.com>
2024-08-09 09:31:15 +0700

    9/8/24 - Thực hành tính diện tích HCN

---
+++ baitap1/BaiTap/BaiTapJavaScript/Su_dung_toan_tu/Tinh_dien_tich_HCN.js
@@ -0,0 +1,4 @@
+var inputWidth = Number(prompt("Enter the width"));
+var inputHeight = Number(prompt("Enter the height"));
+var area = inputWidth * inputHeight;
+document.write("The area is: " + area);
